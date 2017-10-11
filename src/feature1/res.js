0b93191c3622c278887acdf654f34a0dc2c7707c
brittanecrow <bec4za@virginia.edu>
2017-10-11 16:25:17 -0400

    First successful push
    Should be working just fine for LaunchRequest and sample
    FeatureOneRequest

--- src/feature1/res.js
+++ src/feature1/res.js
@@ -1,10 +1,14 @@
 // res.js
 // handles user input and response
 
-function sendResponse(){
-  this.emit(":tell", "Sorry, I don't have that functionality yet.");
+var Alexa = require('alexa-sdk');
+
+function sendResponse() {
+  console.log(this);
+  //TEST: this.emit(':tell', 'Sorry, I don\'t have that functionality yet.');
+  return ('sorry, I don\'t have that functionality yet.');
 }
 
 module.exports = {
-  sendResponse: sendResponse
+  sendResponse: sendResponse(),
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@
 // Alexa Dev Team Fall 2017
 // Code for Good @UVa
 
-let feature1 = require('./feature1/res.js');
+'use strict';
 
+// load additional files/modules needed
+const feature1 = require('./feature1/res.js');
+const Alexa = require('alexa-sdk');
+
+// define constants
+const HELP_MESSAGE = "Welcome to Cav Assistant. For information on my capabilities, ask me to tell you what I can do.";
+const HELP_REPROMPT = "Hi there. Ask me to tell you a U.V.A. tradition or for the phone number of a place nearby.";
+const STOP_MESSAGE = "Thanks for talking. Wah who wah.";
+const APP_ID = 'amzn1.ask.skill.e7b4f881-c503-4c43-bc24-29c242ed0890';
+
+// set up handler to direct intent to proper content
 const handlers = {
     // LaunchRequest when user says 'Alexa, open Cav Assistant'
     'LaunchRequest': function () {
-      this.emit('feature1');
+      this.emit(':ask', "Welcome to Cav Assistant. What can I do for you?");
+      //TEST: this.emit('FeatureOneIntent');
     },
     // Feature1Intent when user says 'Show feature one.'
-    'Feature1Intent': function () => {
-      feature1.sendResponse();
+    'FeatureOneIntent': function () {
+      let speechOutput = feature1.sendResponse;
+      this.emit(':tell', speechOutput);
     },
     // HelpIntent when user says 'Help'
     'AMAZON.HelpIntent': function () {
@@ -27,4 +40,13 @@ const handlers = {
     'AMAZON.StopIntent': function () {
       this.emit(':tell', STOP_MESSAGE);
     }
+};
+
+// Give the handler object and it's functions to the lambda for
+// execution
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context); // create alexa obj
+    alexa.appId = APP_ID; // register app id
+    alexa.registerHandlers(handlers); // register handlers
+    alexa.execute(); // begin
 };
