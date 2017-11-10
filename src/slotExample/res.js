49337023e90fad52ce08fe5782b79285b6cb5dd9
Kennmle <kennmle@gmail.com>
2017-11-09 20:06:58 -0500

    Fixed Issues with slotExample
    Changed keyword 'let' to var; may have been going out of scope before

--- src/slotExample/res.js
+++ src/slotExample/res.js
@@ -5,7 +5,7 @@ module.exports = {
   sendResponse: function sendResponse(slotVal) {
     console.log(this);
     //TEST: this.emit(':tell', 'Sorry, I don\'t have that functionality yet.');
-    let theSlotValue = slotVal;
+    var theSlotValue = slotVal;
     return (theSlotValue);
   }
 }
