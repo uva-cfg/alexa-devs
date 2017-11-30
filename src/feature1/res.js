757dfb5fd15df6957a63a023f69388a6fc0bd219
Samuel Spelsberg <spelsbergsam@gmail.com>
2017-11-29 19:02:51 -0500

    Feature working, testing matching what alexa picks up to our JSON

--- src/feature1/res.js
+++ src/feature1/res.js
@@ -8,9 +8,7 @@ module.exports = {
   sendResponse: function sendResponse(slotValue) {
     var res = "I didn't find anything";
     for (var key in colloqs) {
-      console.log( key );
       if (key == slotValue) {
-        console.log('YOU SUCCESSFULLY FOUND A DESCRIPTION');
         res = colloqs[key]; //check later to make sure that the intent that we are getting lines up with the keys
       }
     }
