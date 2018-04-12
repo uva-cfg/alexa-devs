838a82ac555156602596e83f5ac5e84631169c64
Robert Hails <rlh2ph@virginia.edu>
2018-04-12 11:26:14 -0400

    commented

--- src/louslist_professor/res.js
+++ src/louslist_professor/res.js
@@ -1,15 +1,15 @@
-res.js
-handles user input and response
+// res.js
+// handles user input and response
 
-var Alexa = require('alexa-sdk');
-var classes = require('./fake.json');
-
-module.exports = {
-  sendResponse: function sendResponse(department_abbreviation, course_num) {
-    var res = "The department is " + department_abbreviation + " and the course is " + course_num;
-    console.log("The department_abbreviation is: " + department_abbreviation);
-    console.log("The course_num is: " + course_num);
-    return res;
+// var Alexa = require('alexa-sdk');
+// var classes = require('./fake.json');
+//
+// module.exports = {
+//   sendResponse: function sendResponse(department_abbreviation, course_num) {
+//     var res = "The department is " + department_abbreviation + " and the course is " + course_num;
+//     console.log("The department_abbreviation is: " + department_abbreviation);
+//     console.log("The course_num is: " + course_num);
+//     return res;
     //console.log("The slot value is: " + slotVal);
 
   //   var res = "Sorry, I couldn't find meal exchange options for that location.";
